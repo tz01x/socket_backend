54fef0fcea12bbad4cb884b92c3178adab64006c
tz01x <abudr963rahman@gmail.com>
2022-09-24 21:38:29 +0600

    allow cross origin all

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(expressApp);
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:8000','http://localhost:5500'],
+    origin: '*',
   }
 })
 
