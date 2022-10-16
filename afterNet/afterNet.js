80f003a2004db1af8a3efd3463167e73d911f9cd
tz01x <abudr963rahman@gmail.com>
2022-10-16 18:12:29 +0600

    event for user active status and notification event added

--- afterNet/afterNet.js
+++ afterNet/afterNet.js
@@ -1,11 +1,12 @@
 const { Socket, Namespace } = require("socket.io");
 const fetch = require('node-fetch');
+const { throws } = require("assert");
 
 
-async function  saveMessage(data) {
+async function saveMessage(data) {
     try {
 
-        const response = await fetch('https://tumzied.pythonanywhere.com/afternet/add-message', {
+        const response = await fetch(`${process.env.API_ENDPOINT}/afternet/add-message`, {
             method: 'POST',
             body: JSON.stringify(data),
             headers: {
@@ -15,69 +16,124 @@ async function  saveMessage(data) {
         const jsonData = await response.json();
         console.log(jsonData)
     } catch (error) {
-        console.log('[error]',error)
+        console.log('[error]', error)
     }
 
 }
 
-class AfterNetSocket{
+const socketIdToUid = {};
+const uidActiveMapper = {};
+
+
+class AfterNetSocket {
     /**
      * 
      * @param {Namespace} io 
      * @param {Socket} socket 
      * @returns 
      */
-    constructor(io,socket){
+    constructor(io, socket) {
         this.io = io;
         this.socket = socket;
         this.roomId = ''
-        console.log('[AfterNet.newInstanceCreated] ',socket.id);
+        this.uid = null;
+        this.connected_with_uid = null;
+        console.log('[AfterNet.newInstanceCreated] ', socket.id);
         this.markEventFunctions();
-        
+
     }
 
-    on_setRoom(data){
-        const {roomId} = data;
-        if (roomId){    
+    on_setRoom(data) {
+        const { roomId, uid, connected_with_uid } = data;
+        if (roomId && uid && connected_with_uid) {
             this.socket.join(roomId);
             this.roomId = roomId;
-            console.log('room set to ',roomId);
-            // const sockets =await this.socket.in(roomId).allSockets();
-            // console.log(this.socket.id,sockets.size)
-            // this.socket.emit('receiveMessage',{"from":'Alex',"message":"Hello Tumzied"});
+            this.uid = uid;
+            this.connected_with_uid = connected_with_uid;
+            console.log('room set to ', roomId);
         }
     }
 
-    on_sendMessage(data){
-        if(!this.roomId)
+    async on_sendMessage(data) {
+        if (!this.roomId)
             return;
-        console.log('[emit "send_message"] ',data);
+        console.log('[emit "send_message"] ', data);
         // this.socket.join(this.roomId);
-        if(this.roomId){
-            this.io.to(this.roomId).emit('receiveMessage',data);
+        if (this.roomId) {
+            this.io.to(this.roomId).emit('receiveMessage', data);
             this.addedMessage(data);
+            const sockets = await this.socket.in(this.roomId).allSockets();
+            if (sockets.size == 1) {
+                if (this.connected_with_uid in uidActiveMapper) {
+                    this.socket.to(uidActiveMapper[this.connected_with_uid])
+                        .emit('notification', {
+                            type: 'notify',
+                            reloadRequired: false,
+                            content: `'${data?.displayName}' send a message`,
+                        })
+                }
+            }
         }
     }
 
-    on_disconnect(data){
-        console.log('[AfterNet.Disconnect] ',data);
-    }
+    on_sendNotification(data) {
 
-    markEventFunctions(){
-        Object.getOwnPropertyNames(this.__proto__)
-        .forEach(method_name=>{
-            if(method_name.startsWith('on_')){
-                const func = this.__proto__[method_name];
-                this.socket.on(
-                    method_name.slice(3), // on_abc -> abc
-                    (params)=>func.call(this,params)
-                )
+        const { to, content } = data;
+        if (to && content) {
+            if (to in uidActiveMapper) {
+                this.socket.to(uidActiveMapper[to])
+                    .emit('notification', data)
             }
-        })
-        
+        }
     }
 
-    addedMessage(data){
+    on_setActiveUser(data) {
+        // console.log('[AfterNet.setActiveIsCalled]',data);
+        const { uid } = data;
+        if (!!uid) {
+            this.uid = uid;
+            socketIdToUid[this.socket.id] = uid;
+            uidActiveMapper[uid] = this.socket.id;
+        }
+    }
+
+    on_getUserStatus({ uid }) {
+
+        // console.log('[AfterNet.on_getUserStatus] ',uid);
+        this.socket.join(this.uid);
+        this.io.to(this.uid).emit('receiveUserState', uid && uid in uidActiveMapper);
+        // this.socket.leave(this.uid);
+    }
+
+    on_disconnect(data) {
+        // console.log('[AfterNet.Disconnect] ',data);
+        if (this.socket.id in socketIdToUid) {
+            try {
+                const uid = socketIdToUid[this.socket.id];
+                delete uidActiveMapper[uid];
+                delete socketIdToUid[this.socket.id];
+            } catch (e) {
+                console.log('[AfterNet.Disconnect.Error] ', e);
+            }
+        }
+
+    }
+
+    markEventFunctions() {
+        Object.getOwnPropertyNames(this.__proto__)
+            .forEach(method_name => {
+                if (method_name.startsWith('on_')) {
+                    const func = this.__proto__[method_name];
+                    this.socket.on(
+                        method_name.slice(3), // on_abc -> abc
+                        (params) => func.call(this, params)
+                    )
+                }
+            })
+
+    }
+
+    addedMessage(data) {
         saveMessage(data);
     }
 }
@@ -89,7 +145,7 @@ module.exports = {
      * @param {Socket} socket 
      * @returns 
      */
-    afterNetSocket:(io,socket)=>{
-        return new AfterNetSocket(io,socket)
+    afterNetSocket: (io, socket) => {
+        return new AfterNetSocket(io, socket)
     }
 }
