7555e74afa3433ffb9558c8ddd09a4e362772a03
tz01x <abudr963rahman@gmail.com>
2023-12-18 12:16:34 +0600

    update verifyAccessToken logic

--- afterNet/afterNet.js
+++ afterNet/afterNet.js
@@ -46,12 +46,12 @@ async function get_user_info(uid, token) {
 
 function verifyAccessToken(token, uid) {
     try {
-        console.log(`verifying token with token=${token} uid=${uid} se=${process.env.SECRET_KEY}`)
-        const payload = jwt.verify(token, process.env.SECRET_KEY, { algorithms: ['HS256']});
-        console.log('jwt payload are',payload)
+        // console.log(`verifying token with token=${token} uid=${uid} se=${process.env.SECRET_KEY}`)
+        const payload = jwt.decode(token);
+        // console.log('jwt payload are',payload)
         if (payload?.type == process.env.TOKEN_TYPE) {
-            const user_info = jwt.verify(payload.user_identifier, process.env.PREFIX + process.env.SECRET_KEY);
-            console.log('isValid will get :', user_info.uid == uid , ` for token.uid ${user_info.uid} and uid ${uid}`)
+            const user_info = jwt.decode(payload.user_identifier);
+            // console.log('isValid will get :', user_info.uid == uid , ` for token.uid ${user_info.uid} and uid ${uid}`)
             return user_info.uid == uid
         }
     } catch (e) {
